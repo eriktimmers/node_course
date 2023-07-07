4fbcc8121861792aca916bd877f730563cfd93c4
Erik Timmers <erik.timmers@kega.nl>
2023-07-07 14:20:59 +0200

    [async] use named functions

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,11 +1,5 @@
 console.log('before');
-user = getUser(1, (usr) => {
-    console.log(usr);
-    getRepositories(usr.githubUser, (listOfRepositories) => {
-        console.log('   All repositories ', listOfRepositories);
-
-    });
-});
+user = getUser(1, displayUser);
 console.log('after');
 
 function getUser(id, callback) {
@@ -15,9 +9,28 @@ function getUser(id, callback) {
     }, 2000);
 }
 
+function displayUser(usr) {
+    getRepositories(usr.githubUser, displayRepos);
+}
+
 function getRepositories(username, callback) {
     setTimeout(() => {
         console.log(` Calling github for ${username} ... `);
         callback(['repo1', 'repo2', 'repo3']);
     }, 2000);
+}
+
+function displayRepos(listOfRepositories) {
+    getCommits(listOfRepositories, displayCommits);
+}
+
+function getCommits(repos, callback) {
+    setTimeout(() => {
+        console.log(` Calling github for commits ... `);
+        callback(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+}
+
+function displayCommits(commits) {
+    console.log('   All commits ', commits);
 }
