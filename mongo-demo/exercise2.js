f877fb980219292efd2793eea6f83bce854aa51d
Erik Timmers <erik.timmers@kega.nl>
2023-07-19 16:36:15 +0200

    [mongodb] Basics

---
+++ mongo-demo/exercise2.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mongo-exercises')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB!', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    isPublished: Boolean,
+    price: Number,
+    tags: [ String ],
+    date: Date
+});
+const Course = mongoose.model('Course', courseSchema);
+
+async function getCourses() {
+    return await Course
+        .find({ isPublished: true, tags: { $in: ['frontend', 'backend'] } })
+        .sort({ price: -1 })
+        .select({ name: 1, author: 1 });
+}
+
+async function run() {
+    const courses = await getCourses();
+    console.log(courses);
+}
+
+run();
--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -26,4 +26,36 @@ async function createCourse() {
     console.log(result);
 }
 
-createCourse();
+async function getCourses() {
+    const pageNumer = 2;
+    const pageSize = 10;
+
+
+    const courses = await Course
+        .find({ author: 'Mosh', isPublished: true })
+        .skip((pageNumber - 1) * pageSize)
+        .limit(10)
+        .sort({ name: 1 })
+        .count();
+        // .select({ name: 1, tags: 1 })
+    console.log(courses);
+}
+
+
+async function updateCourse(id) {
+    const course = await Course.findById(id)
+    if (!course) return;
+
+    course.isPublished = true;
+    course.author = 'Another Author';
+
+    // course.set({
+    //     isPublished: true,
+    //     author: 'Yet Another Author'
+    // });
+
+    const result = await course.save();
+    console.log(result);
+}
+
+updateCourse('64b7d5fc1a63424739680dd8');
